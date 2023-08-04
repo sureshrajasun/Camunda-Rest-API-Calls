79a442e55700d3756108bb8286640c785da04b55
sureshrajasingaravadivelu <sureshraja.sun@gmail.com>
2023-08-04 17:52:57 +1000

    Added Json parsing flow

--- fetch.groovy
+++ fetch.groovy
@@ -5,16 +5,17 @@ def firstName = execution.getVariable("firstName")
 def lastName = execution.getVariable("lastName")
 def mobile = execution.getVariable("mobile")
 def id = execution.getVariable("id")
+
 def isValidUser = false
 
 RESTClient client = new RESTClient("http://localhost:8080")
 def path = "/customer/"+id
 def response
+
 try {
     println(path)
     response = client.get(path: path)
     println(response.contentAsString)
-
 
     if(response.contentAsString.contains(firstName)){
         isValidUser = true;
@@ -28,8 +29,17 @@ try {
         isValidUser = true;
     }
 
-    println(isValidUser)
+    println("IS Valid User : " + isValidUser)
 
+    if(isValidUser){
+        def slurper = new groovy.json.JsonSlurper()
+        def result = slurper.parseText(response.contentAsString)
+        println("First Name :" + result.firstName)
+        println("User Id :" + result.id)
+        execution.setVariable("userId", result.id)
+    }
+
+    //return variables
     execution.setVariable("isValidUser", isValidUser)
 
 } catch (RESTClientException e) {
